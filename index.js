191972a28d55b34e5fc2cafff2d5266bc75ff11b
Jan Grzegorowski <grzegorowski@gmail.com>
2019-04-07 10:50:49 +0200

    Add ENV configuration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('ok');
+const express = require('express');
+const path = require('path');
+// configuration
+require('dotenv').config({ path: path.join(__dirname, 'config', 'app.env') });
+
+const app = express();
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at http://localhost:${process.env.PORT}`);
+});
+
